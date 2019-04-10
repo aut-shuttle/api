95d5c336e27b865f6807527e7c2bb0a18a3b3f64
Olaf Wrieden <42084239+olafwrieden@users.noreply.github.com>
2019-04-09 17:42:04 -0700

    Comment Formatting

--- app/routes/Users.js
+++ app/routes/Users.js
@@ -2,18 +2,18 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers').user
 
-//Create User Route
+// Create User Route
 router.post('/', userController.createUser)
 
-//Read all Users
+// Read all Users
 router.get('/', userController.getAll)
 
 // Read one user by ID
 router.get('/:id', userController.getOne)
 
-//Update one user by ID
+// Update one user by ID
 
-//Delete one user by ID
+// Delete one user by ID
 
 // // Old index route kept here for fault finding when I break things
 // router.get('/', (req, res) =>
